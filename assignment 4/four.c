93e1a60e4ad1cab3db5b798ab20acf0558ddb377
Bhushan <waghdharebhushan71@gmail.com>
2024-02-14 00:33:02 +0530

    assignment 4 uploaded

---
+++ assignment 4/four.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    // Method 1
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     printf("%d\n", 2 * i - 1);
+    // }
+
+    // Method 2
+    for (int i = 1; i <= 20; i += 2)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
