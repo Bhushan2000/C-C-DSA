649f8ba867d4aa115eed78c7feb8b20a3f758321
Bhushan <waghdharebhushan71@gmail.com>
2024-02-22 19:19:28 +0530

    hcf new appoach added

--- assignment 11/two.c
+++ assignment 11/two.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 int HCF_of_two_number(int, int);
+int hcf(int, int);
 int main()
 {
-    int number1 = 60, number2 = 75;
-    printf("HCF of %d and %d is %d ", number1, number2, HCF_of_two_number(number1, number2)); // ans 15
+    int number1 = 4, number2 = 6;
+    printf("HCF of %d and %d is %d ", number1, number2, hcf(number1, number2)); // ans 15
     return 0;
 }
+// My Approach
 int HCF_of_two_number(int number1, int number2)
 {
     int min, max, rem;
@@ -27,4 +29,17 @@ int HCF_of_two_number(int number1, int number2)
         rem = max % min;
     }
     return min;
+}
+// Teacher Approach
+int hcf(int n1, int n2)
+{
+    int H;
+    for (H = n1 < n2 ? n1 : n2; H >= 1; H--)
+    {
+        if (n1 % H == 0 && n2 % H == 0)
+        {
+            break;
+        }
+    }
+    return H;
 }
--- assignment 7/four.c
+++ assignment 7/four.c
@@ -1,37 +1,51 @@
 #include <stdio.h>
+void hcf(int, int);
 int main()
 {
     int a, b, small, large, remainder;
     printf("Enter the two number:");
     scanf("%d%d", &a, &b);
-
+    hcf(a, b);
     // hcf by division method (My Approach)
     // check smaller and larger
-    if (a > b)
-    {
-        small = b;
-        large = a;
-    }
-    else
-    {
-        small = a;
-        large = b;
-    }
+    // if (a > b)
+    // {
+    //     small = b;
+    //     large = a;
+    // }
+    // else
+    // {
+    //     small = a;
+    //     large = b;
+    // }
 
-    remainder = large % small;
-    if (remainder)
-    {
-        // repeat this until remainder becomes zero
-        while (remainder != 0)
-        {
-            large = small;  
-            small = remainder;
-            remainder = large % small;
-        }
-        printf("HCF is %d", small);
-    }
-    else
-        printf("HCF is %d", small);
+    // remainder = large % small;
+    // if (remainder)
+    // {
+    //     // repeat this until remainder becomes zero
+    //     while (remainder != 0)
+    //     {
+    //         large = small;
+    //         small = remainder;
+    //         remainder = large % small;
+    //     }
+    //     printf("HCF is %d", small);
+    // }
+    // else
+    //     printf("HCF is %d", small);
 
     return 0;
+}
+// (Teacher Approach)
+void hcf(int n1, int n2)
+{
+    int H;
+    for (H = n1 < n2 ? n1 : n2; H >= 1; H--)
+    {
+        if (n1 % H == 0 && n2 % H == 0)
+        {
+            break;
+        }
+    }
+    printf("HCF is %d", H);
 }
