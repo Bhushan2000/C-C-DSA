b1d5b8951f3c5eecd8b0fde88f7139706dbfb60e
Bhushan <waghdharebhushan71@gmail.com>
2024-02-24 09:37:59 +0530

    updated code for nextPrime()

--- assignment 11/four.c
+++ assignment 11/four.c
@@ -2,18 +2,22 @@
 int nextPrime(int);
 int main()
 {
-    int number = 3;
+    int number = 11;
     printf("Next prime is %d", nextPrime(number));
     return 0;
 }
 int nextPrime(int n)
 {
     int i;
-    for (i = 2; i <= n - 1; i++)
+    while (1)
     {
-        if (n % i == 0)
-            break;
+        n++; // we increase number by one and check next prime or not
+        for (i = 2; i <= n - 1; i++)
+        {
+            if (n % i == 0)
+                break;
+        }
+        if (n == i)
+            return n;
     }
-    if (i == n)
-        return i;
 }
