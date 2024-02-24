82f2a449af5d328254068438ab4dd5b275b143c7
Bhushan <waghdharebhushan71@gmail.com>
2024-02-24 10:01:59 +0530

    Fixed operator associativity

--- assignment 9/ten.c
+++ assignment 9/ten.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
-    int a, b, c, d;
+    int a, b, c, d, root1, root2;
     printf("Enter the values of quadratic equations:");
     scanf("%d %d %d", &a, &b, &c);
-    d = b * b - 4 * a * c;
+    d = (b * b) - (4 * a * c);
     switch (d > 0)
     {
     case 0:
-        if (d < 0)
-            printf("Roots are imaginary ");
-        else
-            printf("Roots are real and equal ");
+        switch (d < 0)
+        {
+        case 0:
+            root1 = (-(b) + sqrt(d)) / (2 * a);
+            root2 = (-(b)-sqrt(d)) / (2 * a);
+            printf("Roots are %d i and %d i", root1, root2);
+            break;
+        case 1:
+            root1 = -(b) / (2 * a);
+            printf("Roots is %d", root1);
+            break;
+        }
         break;
     case 1:
-        printf("Roots are real and distinct ");
+        root1 = (-(b) + sqrt(d)) / (2 * a);
+        root2 = (-(b)-sqrt(d)) / (2 * a);
+        printf("Roots are %d and %d", root1, root2);
         break;
     }
     return 0;
