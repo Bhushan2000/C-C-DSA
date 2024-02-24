4fb8941af13131b86f8e2049ccbb88f35b592e45
Bhushan <waghdharebhushan71@gmail.com>
2024-02-24 09:22:03 +0530

    updated with new approah

--- assignment 7/eight.c
+++ assignment 7/eight.c
@@ -1,24 +1,45 @@
 #include <stdio.h>
+int nextPrime(int);
 int main()
 {
-    int number, i, j;
-    printf("Enter the number:");
-    scanf("%d", &number);
-    // My Approach
-    for (i = number + 1; i <= number + 2; i++)
+    int number = 6, i, j;
+    // printf("Enter the number:");
+    // scanf("%d", &number);
+    printf("The next prime number is %d ", nextPrime(number));
+    // My Approach(Jugadu) // according to my way both are right.
+    // for (i = number + 1; i <= number + 2; i++)
+    // {
+    //     for (j = 2; j < i; j++)
+    //     {
+    //         if (i % j == 0)
+    //             break;
+    //     }
+
+    //     if (i == j)
+    //     {
+    //         // next prime number
+    //         printf("Next Prime number is %d", i);
+    //         break;
+    //     }
+    // }
+
+    return 0;
+}
+// new approach// teacher approach
+// tip: increase number by 1 and then check next prime or not.
+int nextPrime(int number)
+{
+    int i;
+    while (1)
     {
-        for (j = 2; j < i; j++)
+        number++;
+        for (i = 2; i <= number - 1; i++)
         {
-            if (i % j == 0)
+            if (number % i == 0)
                 break;
         }
-
-        if (i == j)
-        {
-            // next prime number
-            printf("Next Prime number is %d", i);
-            break;
-        }
+        if (number == i)
+            // no number divide the given number then prime
+            return number;
     }
-    return 0;
 }
