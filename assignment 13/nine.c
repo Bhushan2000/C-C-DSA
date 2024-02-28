9e1e302a487b0065b528e34c5928a2dadd7e9941
Bhushan <waghdharebhushan71@gmail.com>
2024-02-29 00:26:08 +0530

    Q . nine done

--- assignment 13/nine.c
+++ assignment 13/nine.c
@@ -2,14 +2,13 @@
 int count_digits_number(int);
 int main()
 {
-    printf("%d", count_digits_number(123));
+    printf("Number of digits : %d", count_digits_number(123456));
     return 0;
 }
 int count_digits_number(int n)
 {
     int count;
-    if (n == 1)
-        return 1;
-    count = n + count_digits_number(n / 10);
-    return count;
+    if (n == 0)
+        return 0;
+    return count_digits_number(n / 10) + n / n;
 }
