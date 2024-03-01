8785ed2577a679a0e96475202285237f9d166346
Bhushan <waghdharebhushan71@gmail.com>
2024-03-01 20:30:09 +0530

    Good Quality Questions on recursion

--- assignment 13/eight.c
+++ assignment 13/eight.c
@@ -2,18 +2,26 @@
 int n_terms_fibonacci(int);
 int main()
 {
-    printf("%d", n_terms_fibonacci(3));
+    int number = 10;
+    for (int i = 0; i < number; i++)
+        printf("%d ", n_terms_fibonacci(i));
     return 0;
 }
 int n_terms_fibonacci(int n)
 {
-    int a, b, c;
-    if (n <= 1)
-        return 1;
-    a = n;
-    b = n_terms_fibonacci(n - 1);
-    c = a + b;
-    a = b;
-    b = c;
-    printf("%d ", c);
+    // My Approach
+    // int a, b, c;
+    // if (n <= 1)
+    //     return 1;
+    // a = n;
+    // b = n_terms_fibonacci(n - 1);
+    // c = a + b;
+    // a = b;
+    // b = c;
+    // printf("%d ", c);
+
+    // Teacher approach
+    if (n == 0 || n == 1)
+        return n;
+    return n_terms_fibonacci(n - 1) + n_terms_fibonacci(n - 2);
 }
--- assignment 13/seven.c
+++ assignment 13/seven.c
@@ -2,11 +2,15 @@
 int hcf_of_two(int, int);
 int main()
 {
-    printf("%d", hcf_of_two(4, 6));
+   // printf("HCF is %d", hcf_of_two(36, 60));
+    printf("HCF is %d", hcf_of_two(10, 15));
+   // printf("HCF is %d", hcf_of_two(35, 10));
+   // printf("HCF is %d", hcf_of_two(31, 2));
     return 0;
 }
 int hcf_of_two(int n1, int n2) // 4 ,6
 {
+    // My Approach
     // for (int i = n1 < n2 ? n1 : n2; i > 1; i--) // bigger number can not divide smaller number
     // {                                           // i = 24;                24>1;  i--
     //     if (n1 % i == 0 && n2 % i == 0)
@@ -14,7 +18,7 @@ int hcf_of_two(int n1, int n2) // 4 ,6
     //         return i;
     //     }
     // }
-    int min = n1 < n2 ? n1 : n2, chek;
+
     // while (i != 1)
     // {
     //     if (i == 1)
@@ -27,5 +31,13 @@ int hcf_of_two(int n1, int n2) // 4 ,6
     //     else
     //         i--;
     // }
-    
+
+    // By using teacher approach
+    int min, max;
+    // by using Euclidean Algorithm
+    min = n1 < n2 ? n1 : n2;
+    max = n1 > n2 ? n1 : n2;
+    if (max % min == 0)
+        return min;
+    return hcf_of_two(max - min, min);
 }
