c7c01e300ff631d2eda0d04353357e4687b7f2c4
Bhushan <waghdharebhushan71@gmail.com>
2024-03-03 22:30:09 +0530

    code updated

--- assignment 17/nine_sort_string_array_asc.c
+++ assignment 17/nine_sort_string_array_asc.c
@@ -4,7 +4,7 @@ int main()
 {
 	// char str[] = "String@123";
 	char str[] = "javascript"; // Ans : aacijprstv
-	int smallest, temp, i, counter;
+	int temp, i, counter;
 	// Bubble
 	// selection character
 	for (counter = 0; counter != strlen(str); counter++)
