fc808f62fb4e767147ed283c975a3a569932ab78
Bhushan <waghdharebhushan71@gmail.com>
2024-03-03 22:28:59 +0530

    Solved find first 1 in LSB

--- assignment 2/seven_find_the_position_of_first_1_in_LSB.c
+++ assignment 2/seven_find_the_position_of_first_1_in_LSB.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
+#include <math.h>
+int position_of_last_bit(int);
 int main()
 {
-    int no = 18, position = 0, binary = 0, rem = 0;
+    int no = 19;
     // printf("Enter the no:");
     // scanf("%d", &no);
     //  enter your code here.
-    while (no != 1)
-    {
-        rem = no % 2;
-        no = no / 2;
-    }
-    printf("Position of the first 1 in LSB is %d", rem);
+    //printf("%d", no & -no);
+    // take 2's compliment of the number
+
+    printf("Position of the first 1 in LSB is %d", position_of_last_bit(no));
     return 0;
 }
+int position_of_last_bit(int n)
+{
+    return log2(n & -n) + 1;
+}
+// Input: n = 18
+// Output: 2
+// Explanation: Binary Representation of 18 is 010010, hence position of first set bit from right is 2.
