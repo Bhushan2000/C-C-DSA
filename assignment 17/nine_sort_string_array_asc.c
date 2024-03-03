66d64babf540f61f6e70611ec32fe3af542c5016
Bhushan <waghdharebhushan71@gmail.com>
2024-03-03 15:04:10 +0530

    String array sorting

--- assignment 17/nine_sort_string_array_asc.c
+++ assignment 17/nine_sort_string_array_asc.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
+#include <string.h>
 int main()
 {
     char str[] = "String@123";
-    for (int i = 0; str[i]; i++)
+    int smallest = str[0], temp, position, counter = 0, i;
+    while (counter != strlen(str) - 1)
     {
+        smallest = str[counter];
+        for (i = 0; i < strlen(str) - 1; i++)
+        { // select smallest
+            if (smallest < str[i + 1])
+            {
+            }
+            else
+            {
+                smallest = str[i + 1];
+                position = i + 1;
+            }
+        }
+        if (str[counter] > str[position])
+        {
+            // swap
+            temp = str[position];
+            str[position] = str[counter];
+            str[counter] = temp;
+        }
+        // change counter
+        if (i == strlen(str)-1)
+            counter++;
     }
     printf("After Sort on String %s", str);
     return 0;
