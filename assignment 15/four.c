91dbfffd93b414b19e5425f9e5f4e7a1e0fbe8cc
Bhushan <waghdharebhushan71@gmail.com>
2024-03-04 23:52:20 +0530

    Changes done.

--- assignment 15/four.c
+++ assignment 15/four.c
@@ -3,15 +3,20 @@ void rotate_array_n_d_position(int[], int, int, int);
 int main()
 {
     int size = 5, arr[] = {32, 29, 40, 12, 70}, n = 2;
-    // char d = 'l'; // left or Right
-    char d = 'r'; // left or Right
-    // resulting array after left rotation 2 times is [40, 12, 70, 32, 29]
+    //char d = 'l'; // left or Right
+     char d = 'r'; // left or Right
+    //  resulting array after left rotation 2 times is [40, 12, 70, 32, 29]
     rotate_array_n_d_position(arr, size, n, d);
     return 0;
 }
 void rotate_array_n_d_position(int arr[], int size, int n, int d)
 {
-    // takes time 1:12 hr to think and implement it.
+    printf("Given array : ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    // takes time 2 hr to think and implement it.
     int count = 0, temp;
     for (int i = 0; i < size; i++)
     {
@@ -31,7 +36,7 @@ void rotate_array_n_d_position(int arr[], int size, int n, int d)
                 count = 0;
             }
         }
-        else
+        if (i > n && d == 'r')
         {
             count = size - 1;
             if (i > n && d == 'r')
@@ -52,6 +57,8 @@ void rotate_array_n_d_position(int arr[], int size, int n, int d)
             }
         }
     }
+    printf("\n");
+    printf("After roatate array %d postion in %c direction : ", n, d);
     for (int i = 0; i < size; i++)
     {
         printf("%d ", arr[i]);
