abce7dbfddaedc045a7f64efdd6f42d655e2c710
Bhushan <waghdharebhushan71@gmail.com>
2024-03-05 18:29:11 +0530

    new approach added for armstrong

--- assignment 7/ten.c
+++ assignment 7/ten.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <math.h>
+void armstrong();
 int main()
 {
     int no, rem, x;
@@ -9,7 +11,6 @@ int main()
         x = 0;
         while (no != 0)
         {
-            /* code */
             rem = no % 10;
             x += rem * rem * rem;
             no = no / 10;
@@ -31,6 +32,36 @@ int main()
         // if (x == i)
         //     printf("%d ", x);
     }
-
+    printf("\n");
+    armstrong();
     return 0;
+}
+// teacher approach
+void armstrong()
+{
+    int n = 1000, a, b, count = 0, rem, sum = 0;
+    a = n;
+    while (a)
+    {
+        b = a;
+        while (b)
+        {
+            b = b / 10;
+            count++;
+        }
+        b = a;
+        while (b)
+        {
+            rem = b % 10;
+            sum = sum + pow(rem, count);
+            b = b / 10;
+        }
+        if (sum == a)
+            printf("%d ", sum);
+
+        sum = 0;
+        rem = 0;
+        count = 0;
+        a--;
+    }
 }
