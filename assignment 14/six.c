abbb1922aee0a3a0b2bf0dfe230dde986c6a104b
Bhushan <waghdharebhushan71@gmail.com>
2024-03-05 19:31:58 +0530

    Sort array simple code added

--- assignment 14/six.c
+++ assignment 14/six.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 void input(int[], int);
 void sortarray(int[], int);
+void sortArray_simple_way(int[], int);
 int main()
 {
-    int size = 10, arr[] = {10, 2222, 333, 44, 5}, arr1[] = {10, 2222, 333, 44, 5, 766, 1000, 80, 9, 100};
+    int size = 5, arr[] = {10, 2222, 333, 44, 5};
+    //  int size = 10,   arr1[] = {10, 2222, 333, 44, 5, 766, 1000, 80, 9, 100};
     // input(arr, size);
-    sortarray(arr1, size);
+    // sortarray(arr1, size);
+    sortArray_simple_way(arr, size);
     return 0;
 }
+
 void input(int arr[], int size)
 {
     printf("Enter the values:");
@@ -16,6 +20,7 @@ void input(int arr[], int size)
         scanf("%d", &arr[i]);
     }
 }
+// myApproach first come in the mind
 void sortarray(int a[], int size)
 {
     int temp, i, arrysize = size;
@@ -44,4 +49,25 @@ void sortarray(int a[], int size)
         printf("%d ", a[i]);
     }
     printf("\nSize is %d", arrysize);
+}
+
+void sortArray_simple_way(int a[], int size)
+{
+    int temp;
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (a[i] > a[j])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", a[i]);
+    }
 }
