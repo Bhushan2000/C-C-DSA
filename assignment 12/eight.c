17f2dbcf205b98e25a0e949c58dd4b666514f19d
Bhushan <waghdharebhushan71@gmail.com>
2024-03-05 17:23:52 +0530

    using bitwise opearator  new approach

--- assignment 12/eight.c
+++ assignment 12/eight.c
@@ -2,17 +2,26 @@
 void binaryofdecimal(int);
 int main()
 {
-    int number = 100;
+    int number = 18;
     // printf("Enter the number:");
     // scanf("%d", &number);
     binaryofdecimal(number);
     return 0;
 }
+// void binaryofdecimal(int n)
+// {
+//     if (n != 0)
+//     {
+//         binaryofdecimal(n / 2);
+//         printf("%d", n % 2);
+//     }
+// }
+// By teacher
+// using bitwise opearator
 void binaryofdecimal(int n)
 {
-    if (n != 0)
-    {
-        binaryofdecimal(n / 2);
-        printf("%d", n % 2);
-    }
+    if (n == 0)
+        return;
+    binaryofdecimal(n >> 1);
+    printf("%d", n & 1);
 }
