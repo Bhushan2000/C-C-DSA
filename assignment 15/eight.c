61dd5427da8af4144bf9333798907634fb013786
Bhushan <waghdharebhushan71@gmail.com>
2024-03-06 23:04:47 +0530

    updation ongoing

--- assignment 15/eight.c
+++ assignment 15/eight.c
@@ -2,20 +2,21 @@
 void unique_elements(int[], int);
 int main()
 {
-    int arr[] = {1, 0, 2, 2, 4,5,5,8,9,1,5,1,54,6};
+    int arr[] = {1, 0, 2, 2, 4};
     int size = sizeof(arr) / 4;
     unique_elements(arr, size);
     return 0;
 }
 void unique_elements(int arr[], int size)
 {
-    int unique;
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size-1; i++)
     {
-        if (arr[i] != unique)
+        for (int j = i + 1; j < size; j++)
         {
-            printf("%d ", arr[i]);
-            unique = arr[i];
+            if (arr[i] != arr[j])
+            {
+                printf("%d ", arr[i]);
+            }
         }
     }
 }
