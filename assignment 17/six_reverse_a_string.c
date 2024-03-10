6e983e1635abe308361754e2a57ff4e850b8717d
Bhushan <waghdharebhushan71@gmail.com>
2024-03-10 22:24:11 +0530

    time complexity O(n/2) take to variables first point to 0th index and second point to last index of the array run loop i<j and the swap numbers with their position.

--- assignment 17/six_reverse_a_string.c
+++ assignment 17/six_reverse_a_string.c
@@ -3,13 +3,31 @@
 int main()
 {
     char str[] = "I am a Powerful Being.";
-    printf("%s\n", str);
-    printf("After reversed is %s\n", strrev(str));
-    // check the condition for reverse the string using for loop
-    // for (int i = 0; str[i]; i++)
+    int count = 0;
+    // printf("After reversed is %s\n", strrev(str));
+    //  check the condition for reverse the string using for loop
+
+    for (int i = 0; str[i]; i++)
+        count++;
+    char strcopy[count];
+    int i = 0, j = count - 1, temp;
+    // for (int j = 0; j < count - 1; j++)
     // {
-    //     printf("%c",str[i]);
+    //     for (int k = count - 1 - j; k >= k+1-j; k--)
+    //     {
+    //         strcopy[j] = str[k];
+    //     }
     // }
-    
+    while (i <= j)
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++;
+        j--;
+    }
+    // reverse string using swapping tc O(n/2)
+
+    printf("%s ", str);
     return 0;
 }
