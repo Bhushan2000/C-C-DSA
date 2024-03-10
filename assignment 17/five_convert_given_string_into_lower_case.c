b931d9ebcd7a5256b96c3510071f08fd04b28ef7
Bhushan <waghdharebhushan71@gmail.com>
2024-03-10 22:26:26 +0530

    Difference between the ASCII Code of A and a is 32 so use this logic

--- assignment 17/five_convert_given_string_into_lower_case.c
+++ assignment 17/five_convert_given_string_into_lower_case.c
@@ -3,7 +3,18 @@
 int main()
 {
     char str[] = "I am a Powerful Being.";
-    printf("%s\n", str);
-    printf("After converted into lowercase is %s", strlwr(str));
+    //printf("%s\n", str);
+    for (int i = 0; str[i]; i++)
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            printf("%c", (str[i] + 32));
+        }
+        else
+        {
+            printf("%c", str[i]);
+        }
+    }
+    //printf("After converted into lowercase is %s", strlwr(str));
     return 0;
 }
--- assignment 17/four_convert_given_string_into_upper_case.c
+++ assignment 17/four_convert_given_string_into_upper_case.c
@@ -3,7 +3,18 @@
 int main()
 {
     char str[] = "I am a Powerful Being.";
-    printf("%s\n", str);
-    printf("After converted into lowercase is %s", strupr(str));
+    // printf("%s\n", str);
+    for (int i = 0; str[i]; i++)
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            printf("%c", (str[i] - 32));
+        }
+        else
+        {
+            printf("%c", str[i]);
+        }
+    }
+    // printf("After converted into lowercase is %s", strupr(str));
     return 0;
 }
