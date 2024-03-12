dba612cefe65d226d55d5e828bf9a2294795fec7
Bhushan <waghdharebhushan71@gmail.com>
2024-03-12 13:54:28 +0530

    new approach added learn by teacher

--- assignment 18/nine_reverse_string_word_wise.c
+++ assignment 18/nine_reverse_string_word_wise.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+void reverse_string_word_by_word();
+void new_approach();
+void swap(char[], int, int);
 int main()
+{
+    new_approach();
+    return 0;
+}
+void reverse_string_word_by_word()
 {
     char str[] = "MySirG Education Services";
     char temp[26] = {}, counter = 0;
-    // “Services Education Mysirg”
+    // output: “Services Education Mysirg”
     int i, j;
     for (i = 0; str[i]; i++)
     {
@@ -28,5 +36,42 @@ int main()
         }
     }
     printf("%s", strrev(temp));
-    return 0;
+}
+// by teacher
+void new_approach()
+{
+    char str[] = "MySirG Education Services";
+    int i = 0, flag = 0, start = 0, end = 0;
+    while (str[i] != '\0') // run until '\0' not found
+    {
+        while (str[i] != ' ') // run until ' ' not find
+        {
+            if (str[i] == '\0')  // check null character or not for last word
+            {
+                flag = 1;
+                break;
+            }
+            end++;
+            i++;
+        }
+        swap(str, start, end - 1);
+        if (flag == 1)
+            break;
+        start = end++;
+        i++;
+    }
+    swap(str, 0, i - 1);
+    printf("%s", str);
+}
+void swap(char str[], int i, int j)
+{
+    char temp;
+    while (i <= j)
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++;
+        j--;
+    }
 }
