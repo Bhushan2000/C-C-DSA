1af771a234201fc4342e40cb5d6c26bca3021771
Bhushan <waghdharebhushan71@gmail.com>
2024-03-18 17:54:00 +0530

    new appraoch added for min distance between two words

--- assignment 19/Eight_min_distance_between_two_words_in_list.c
+++ assignment 19/Eight_min_distance_between_two_words_in_list.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+void new_approach();
 int main()
 {
     // (Example : s = {“the”,”quick”,”brown”,”fox”,”quick”}
@@ -24,6 +26,28 @@ int main()
             break;
         }
     }
-    printf("count for the word : %d", flag);
+    // printf("count for the word : %d", flag);
+    new_approach();
     return 0;
+}
+void new_approach()
+{
+    char str[5][20] = {"my", "name", "name", "is", "prateek"};
+    char word1[10] = "name";
+    char word2[10] = "prateek";
+    int startposition = -1, endpostion = -1, min_difference;
+    for (int i = 0; i < sizeof(str) / sizeof(str[0]); i++)
+    {
+        if (strcmp(str[i], word1) == 0)
+            startposition = i;
+        else if (strcmp(str[i], word2) == 0)
+        {
+            endpostion = i;
+        }
+    }
+    min_difference = abs(endpostion - startposition) - 1;
+    // it return always postive value like mod | w2-w1 | in maths
+    printf("Minimum distance between two words is %d", min_difference);
+    // printf("Minimum distance between two words is %d", (endpostion - startposition) - 1);
+    //  -1 because we start the indexing from zero not from one
 }
