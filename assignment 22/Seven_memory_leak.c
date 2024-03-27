37b0efba5e19642835d2c9a4ac83daae65dbe7fc
Bhushan <waghdharebhushan71@gmail.com>
2024-03-27 15:25:05 +0530

    Memory leak means the DMA variable memory we unable to access using a pointer or we miss the address of that location.

--- assignment 22/Seven_memory_leak.c
+++ assignment 22/Seven_memory_leak.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int *memory_leak();
+// int *memory_leak();
 int main()
 {
-    printf("%d", *memory_leak()); // we lost memory address memory leak
+    // printf("Memory leak : %d", *memory_leak()); // we lost memory address memory leak
+
+    // right way
+    int *ptr;
+    ptr = (int *)malloc(sizeof(int));
+    ptr = NULL;
+    // this is memory leak 
+    // DMA variable is present in memory but we unable to access it.
+    // "Dangling pointer" means we try to access that memory location that is deleted.
     return 0;
 }
-int *memory_leak()
-{
-    int *ptr = NULL;
-    ptr = (int *)malloc(sizeof(int));
-    *ptr = 5;
-    free(ptr);
-    return ptr;
-}
+// int *memory_leak()
+// {
+//     int *ptr = NULL;
+//     ptr = (int *)malloc(sizeof(int));
+//     *ptr = 5;
+//     free(ptr);
+//     return ptr;
+// }
