a6c8dda666440531389646cf7ca85f669bfb3f7f
Bhushan <waghdharebhushan71@gmail.com>
2024-03-27 15:21:29 +0530

    Right Approach added for string input using DMA without memory wastage.

--- assignment 22/One_input_variable_string.c
+++ assignment 22/One_input_variable_string.c
@@ -2,13 +2,15 @@
 #include <string.h>
 #include <stdlib.h>
 char *string_input();
+void newApproach();
 int main()
 {
     char *t;
-    t = string_input();
-    for (int i = 0; *(t + i); i++)
-        printf("%c", *(t + i));
-    free(t);
+    // t = string_input();
+    // for (int i = 0; *(t + i); i++)
+    //     printf("%c", *(t + i));
+    // free(t);
+    newApproach();
     return 0;
 }
 char *string_input()
@@ -18,4 +20,22 @@ char *string_input()
     printf("Enter the string ");
     fgets(st, 30, stdin);
     return st;
+}
+// right approach
+void newApproach()
+{
+    char *ptr, c;
+    int j = 1, i = 0;
+    ptr = (char *)malloc(sizeof(char));  // allocate one byte memory
+    printf("Enter the String:");
+    while (c != '\n')
+    {
+        c = getc(stdin); // accept one character at a time
+        j++;
+        ptr = (char *)realloc(ptr, j * sizeof(char));
+        ptr[i] = c;
+        i++;
+    }
+    ptr[i] = '\0';
+    printf("Resultant string is %s", ptr);
 }
