f0dea5b075b8029256e61f2ddb427643ff608ff7
Bhushan <waghdharebhushan71@gmail.com>
2024-04-10 22:10:45 +0530

    Simplified code added for Pascal triangle

--- assignment 24/Four_Pascal_Triangle.cpp
+++ assignment 24/Four_Pascal_Triangle.cpp
@@ -15,6 +15,8 @@ int main()
 int factorial(int no)
 {
     int fact = 1;
+    if (no <= 0)
+        return 1; // factorial of 0 is 1
     while (no != 0)
     {
         fact *= no;
@@ -22,31 +24,51 @@ int factorial(int no)
     }
     return fact;
 }
+// My Approach
+// void pattern(int lines)
+// {
+//     int k, rcount;
+//     for (int i = 0; i < lines; i++)
+//     {
+//         k = 1;
+//         rcount = 0;
+//         for (int j = 0; j < lines * 2; j++)
+//         {
+//             if (j > lines - 1 - i && j < lines + 1 + i && k == 1)
+//             {
+//                 // printf("*");
+//                 cout << combination(i, rcount);
+//                 rcount++;
+//                 k = 0; // this k variable is for skipping the between stars
+//             }
+//             else
+//             {
+//                 cout << " ";
+//                 k = 1;
+//             }
+//         }
+//         cout << endl;
+//     }
+// }
+
+// Learn from Prateek Sir
 void pattern(int lines)
 {
-    int k, rcount;
+    int k;
     for (int i = 0; i < lines; i++)
     {
-        k = 1;
-        rcount = 0;
-        for (int j = 0; j < lines * 2; j++)
+        for (int j = 0; j <= i; j++) // loop run til 'i' value
         {
-            if (j > lines - 1 - i && j < lines + 1 + i && k == 1)
-            {
-                // printf("*");
-                cout << combination(i, rcount);
-                rcount++;
-                k = 0;
-            }
-            else
-            {
-                cout << " ";
-                k = 1;
-            }
+            k = 0;
+            if (j == 0)
+                while (k++ <= (lines / 2 - i + 1)) // for printing space
+                    printf(" ");
+            printf("%d ", combination(i, j));
         }
-        cout << endl;
+        printf("\n");
     }
 }
+
 int combination(int n, int r)
 {
     int result;
