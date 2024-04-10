5560086d95090baa6486ee8cf3240db5710d088e
Bhushan <waghdharebhushan71@gmail.com>
2024-04-10 13:31:06 +0530

    New approach learn for check prime no condition

--- assignment 24/One_check_prime.cpp
+++ assignment 24/One_check_prime.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 using namespace std;
 void checkPrime(int);
+int primeNumber(int);
 int main()
 {
     int no, i;
     cout << "Enter the number to check prime or not" << endl;
     cin >> no;
-    checkPrime(no);
+    // checkPrime(no);
+
+    if (primeNumber(no))
+        cout << "Number is not Prime";
+    else
+        cout << "Number is Prime";
 }
 void checkPrime(int no)
 {
@@ -18,4 +24,13 @@ void checkPrime(int no)
         cout << "Number is Prime";
     else
         cout << "Number is not Prime";
+}
+// Learn from Prateek Sir
+int primeNumber(int no)
+{
+    int i;
+    for (i = 2; i <= no / 2; i++)
+        if (no % i == 0)
+            return 1; // not prime
+    return 0;         // prime
 }
