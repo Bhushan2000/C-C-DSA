dea393979e0bf4ac24e45a09b325021316e7ca71
Bhushan <waghdharebhushan71@gmail.com>
2024-04-17 23:42:34 +0530

    files updated

--- assignment 24/Eight_overloaded_function.cpp
+++ assignment 24/Eight_overloaded_function.cpp
@@ -9,12 +9,12 @@ int main()
     // rectangle
     // triangle
     int radius, length, width, base, height;
-    cout << "Enter the radius of the circle ";
-    cin >> radius;
-    cout << "Area of circle " << area(radius) << endl;
-    cout << "Enter the length and width of the rectangle ";
-    cin >> length >> width;
-    cout << "Area of rectangle " << area(length, width) << endl;
+    // cout << "Enter the radius of the circle ";
+    // cin >> radius;
+    // cout << "Area of circle " << area(radius) << endl;
+    // cout << "Enter the length and width of the rectangle ";
+    // cin >> length >> width;
+    // cout << "Area of rectangle " << area(length, width) << endl;
     cout << "Enter the base and height of the triangle ";
     cin >> base >> height;
     cout << "Area of triangle " << area(base, height) << endl;
@@ -28,10 +28,10 @@ float area(float radius)
 int area(int length, int width)
 {
     // area of rectangle
-    return length * width;
+    return (length * width);
 }
 float area(float base, float height)
 {
     // area of triangle
-    return 0.5 * base * height;
+    return (0.5 * base * height);
 }
--- assignment 24/Nine_max_of_two_overloading_function.cpp
+++ assignment 24/Nine_max_of_two_overloading_function.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
-int max_of_two(int, int);
+// int max_of_two(int, int);
+float max_of_two(float, float);
 int main()
 {
     int no1, no2;
@@ -10,7 +11,11 @@ int main()
     cout << max_of_two(no1, no2);
     return 0;
 }
-int max_of_two(int a, int b)
+// int max_of_two(int a, int b)
+// {
+//     return a > b ? a : b;
+// }
+float max_of_two(float a, float b)
 {
     return a > b ? a : b;
 }
